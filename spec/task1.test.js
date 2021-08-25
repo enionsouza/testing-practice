863aa4f926cc00ee42d44d90cae122157f1039f5
Enio N. de Souza <enionsouza@gmail.com>
2021-08-24 20:05:50 -0400

    Fix syntax

--- spec/task1.test.js
+++ spec/task1.test.js
@@ -3,10 +3,10 @@ const stringLength = require('../modules/task1');
 describe('Task1 module', () => {
   test('should return the correct length of a string', () => {
     // Arrange
-    input = 'Microverse';
+    const input = 'Microverse';
 
     // Act
-    output = stringLength(input);
+    const output = stringLength(input);
 
     // Assert
     expect(output).toBe(10);
@@ -14,10 +14,10 @@ describe('Task1 module', () => {
 
   test('should throw an error for strings longer than 10 characters', () => {
     // Arrange
-    input = 'JavaScript is awesome';
+    const input = 'JavaScript is awesome';
 
     // Act
-    output = () => stringLength(input);
+    const output = () => stringLength(input);
 
     // Assert
     expect(output).toThrow('String should not be longer than 10 characters');
@@ -25,10 +25,10 @@ describe('Task1 module', () => {
 
   test('should throw an error for empty strings', () => {
     // Arrange
-    input = '';
+    const input = '';
 
     // Act
-    output = () => stringLength(input);
+    const output = () => stringLength(input);
 
     // Assert
     expect(output).toThrow('String should be at least 1 character long');
--- spec/task2.test.js
+++ spec/task2.test.js
@@ -3,10 +3,10 @@ const reverseString = require('../modules/task2');
 describe('Task2 module', () => {
   test('should take a string as an argument and return it reversed', () => {
     // Arrange
-    input = 'JavaScript is awesome!';
+    const input = 'JavaScript is awesome!';
 
     // Act
-    output = reverseString(input);
+    const output = reverseString(input);
 
     // Assert
     expect(output).toBe('!emosewa si tpircSavaJ');
